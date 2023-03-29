370872917c63d238ef150cadda3c6464af9b8d9e
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-03-29 15:19:19 +0200

    Add connection to mysql via mikro-orm

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,30 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import 'reflect-metadata'
 import Koa from 'koa'
 import { createContainer, Lifetime } from 'awilix'
 import { loadControllers, scopePerRequest } from 'awilix-koa'
+import initializeORM from './connection'
 import { config } from './config'
 
-const app = new Koa()
-const container = createContainer()
+initializeORM()
+    .then(async () => {
+        const app = new Koa()
+        const container = createContainer()
 
-const servicesPath: string = './modules/**/*Service.ts'
-container.loadModules([[servicesPath, Lifetime.SCOPED]], {
-    cwd: __dirname,
-    formatName: 'camelCase',
-})
+        const servicesPath: string = './modules/**/*Service.ts'
+        container.loadModules([[servicesPath, Lifetime.SCOPED]], {
+            cwd: __dirname,
+            formatName: 'camelCase',
+        })
 
-app.use(scopePerRequest(container))
+        app.use(scopePerRequest(container))
 
-const controllersPath: string = 'modules/**/*Controller.ts'
-app.use(loadControllers(controllersPath, { cwd: __dirname }))
+        const controllersPath: string = 'modules/**/*Controller.ts'
+        app.use(loadControllers(controllersPath, { cwd: __dirname }))
 
-app.listen(config.port)
+        app.listen(config.port)
+    })
+    .catch((e) => {
+        console.log(e)
+    })

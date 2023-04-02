451b9c7530c37e0f89d641c178b7cf0d3bc05725
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-04-02 17:47:54 +0200

    Add registering entities repositories

--- src/connection.ts
+++ src/connection.ts
@@ -1,6 +1,6 @@
-import { MikroORM } from '@mikro-orm/mysql'
+import { MikroORM, MySqlDriver } from '@mikro-orm/mysql'
 import ormConfig from './mikro-orm.config'
 
-const initializeORM = async () => await MikroORM.init(ormConfig)
+const initializeORM = async () => await MikroORM.init<MySqlDriver>(ormConfig)
 
 export default initializeORM

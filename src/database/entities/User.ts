5dfba8dc8914d2240b968d1f066212247285a644
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-04-02 11:34:18 +0200

    Add user entity

---
+++ src/database/entities/User.ts
@@ -0,0 +1,44 @@
+import { Entity, Enum, Property, Unique } from '@mikro-orm/core'
+import CustomBaseEntity from './CustomBaseEntity'
+import { IsEmail, IsEnum, Length } from 'class-validator'
+import { UserRole } from '../enums'
+
+@Entity()
+export default class User extends CustomBaseEntity {
+    @Property({
+        length: 30,
+    })
+    @Unique()
+    @Length(5, 30)
+    username!: string
+
+    @Property({
+        length: 50,
+    })
+    @Length(3, 50)
+    firstName!: string
+
+    @Property({
+        length: 80,
+    })
+    @Length(3, 80)
+    lastName!: string
+
+    @Property({
+        length: 50,
+    })
+    @Unique()
+    @Length(5, 50)
+    @IsEmail()
+    email!: string
+
+    @Property({
+        length: 50,
+    })
+    @Length(5, 50)
+    password!: string
+
+    @Enum()
+    @IsEnum(UserRole)
+    role!: UserRole
+}

c64e6c47993f8beebf50227aa0440d3801a16889
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-04-09 19:02:15 +0200

    Add auth module & user service

---
+++ src/modules/auth/types/index.ts
@@ -0,0 +1,12 @@
+export type UserData = {
+    username: string
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+}
+
+export type UserSignInData = {
+    username: string
+    password: string
+}
---
+++ src/modules/user/userService.ts
@@ -0,0 +1,55 @@
+import { EntityRepository } from '@mikro-orm/core'
+import bcrypt from 'bcrypt'
+import User from '../../database/entities/User'
+import { UserRole } from '../../database/enums'
+
+export default class UserService {
+    userRepository: EntityRepository<User>
+
+    constructor({ userRepository }: { userRepository: EntityRepository<User> }) {
+        this.userRepository = userRepository
+    }
+
+    public async createUser({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+    }: {
+        username: string
+        firstName: string
+        lastName: string
+        email: string
+        password: string
+    }): Promise<User> {
+        const isEmailAvailable: boolean = await this.checkEmailAvailability(email)
+        if (!isEmailAvailable) {
+            throw new Error('Email is already used')
+        }
+
+        const user: User = new User()
+        user.username = username
+        user.firstName = firstName
+        user.lastName = lastName
+        user.email = email
+        user.isActive = false
+        user.role = UserRole.USER
+
+        const hashedPassword: string = await this.getHashPassword(password)
+        user.password = hashedPassword
+
+        return await this.userRepository.upsert(user)
+    }
+
+    private async checkEmailAvailability(email: string): Promise<boolean> {
+        const userWithGivenEmail: User = await this.userRepository.findOne({ email: email })
+        return !userWithGivenEmail
+    }
+
+    private async getHashPassword(password: string): Promise<string> {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword: string = await bcrypt.hash(password, salt)
+        return hashedPassword
+    }
+}

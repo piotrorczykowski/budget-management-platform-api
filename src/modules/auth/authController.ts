3a91b66410da89e83de20a068312961fc33acb8f
Piotr Orczykowski <228820@edu.p.lodz.pl>
2023-04-09 18:41:27 +0200

    Add auth module & user service

---
+++ src/modules/auth/authController.ts
@@ -0,0 +1,30 @@
+import { Context } from 'koa'
+import { route, POST } from 'awilix-koa'
+import AuthService from './authService'
+import { UserData, UserSignInData } from './types'
+
+@route('/auth')
+export default class AuthController {
+    authService: AuthService
+
+    constructor({ authService }: { authService: AuthService }) {
+        this.authService = authService
+    }
+
+    @route('/signUp')
+    @POST()
+    public async signUp(ctx: Context): Promise<void> {
+        const userData: UserData = <UserData>ctx.request.body
+        await this.authService.signUp(userData)
+        ctx.status = 201
+    }
+
+    @route('/signIn')
+    @POST()
+    public async signIn(ctx: Context): Promise<any> {
+        const { username, password }: UserSignInData = <UserSignInData>ctx.request.body
+        const token: string = await this.authService.signIn(username, password)
+        ctx.body = { accessToken: token }
+        ctx.status = 200
+    }
+}

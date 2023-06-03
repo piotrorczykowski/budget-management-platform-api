6938d19c38c04318e7166203b469a0d2028e0640
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-06-03 11:44:22 +0200

    Add implementation for user activation

--- src/modules/user/usersService.ts
+++ src/modules/user/usersService.ts
@@ -1,12 +1,24 @@
 import { EntityRepository } from '@mikro-orm/core'
 import bcrypt from 'bcrypt'
 import User from '../../database/entities/User'
+import jwt from 'jsonwebtoken'
 import { UserRole } from '../../database/enums'
+import MailsService from '../mail/mailsService'
+import { config } from '../../config'
+import logger from '../../middleware/winston'
 
 export default class UserService {
+    mailsService: MailsService
     userRepository: EntityRepository<User>
 
-    constructor({ userRepository }: { userRepository: EntityRepository<User> }) {
+    constructor({
+        mailsService,
+        userRepository,
+    }: {
+        mailsService: MailsService
+        userRepository: EntityRepository<User>
+    }) {
+        this.mailsService = mailsService
         this.userRepository = userRepository
     }
 
@@ -21,6 +33,7 @@ export default class UserService {
         email: string
         password: string
     }): Promise<void> {
+        logger.info('Creating new user...')
         const isUsernameAvailable: boolean = await this.checkUsernameAvailability(username)
         if (!isUsernameAvailable) {
             throw new Error('Username is already used')
@@ -42,6 +55,8 @@ export default class UserService {
         user.password = hashedPassword
 
         await this.userRepository.persistAndFlush(user)
+
+        await this.sendUserActivationMail(email)
     }
 
     private async checkUsernameAvailability(username: string): Promise<boolean> {
@@ -58,5 +73,24 @@ export default class UserService {
         const salt = await bcrypt.genSalt()
         const hashedPassword: string = await bcrypt.hash(password, salt)
         return hashedPassword
+    }
+
+    private async sendUserActivationMail(email: string): Promise<void> {
+        logger.info(`Sending user activation mail to: ${email}...`)
+        const token: string = this.generateTokenForUser(email)
+        await this.mailsService.sendUserActivationMail(email, token)
+    }
+
+    private generateTokenForUser(email: string): string {
+        const expirationTime: number = 60 * 60 // 1 hour
+        const tokenPayload: { email: string } = { email: email }
+        const token: string = jwt.sign(tokenPayload, config.jwtSecret, { expiresIn: expirationTime })
+        return token
+    }
+
+    public async activateUser(email: string): Promise<void> {
+        const user: User = await this.userRepository.findOneOrFail({ email: email })
+        user.isActive = true
+        await this.userRepository.persistAndFlush(user)
     }
 }

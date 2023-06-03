a7b7a3fb74b24b569626133c3a8a19f35efc5564
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-06-03 11:31:16 +0200

    Refactor mail module

--- src/modules/mail/mailsService.ts
+++ src/modules/mail/mailsService.ts
@@ -19,16 +19,14 @@ export default class MailingService {
         })
     }
 
-    public async sendUserActivationMail(to: string, token: string, userId: number): Promise<void> {
+    public async sendUserActivationMail(to: string, token: string): Promise<void> {
         const mailData: any = {
             from: config.emailFrom,
             to: to,
             subject: 'Account Activation',
             html: `<h2 style="color: black;">Almost done..</h2>
             <p>Thank you for registering with us. In order to activate your account please click the link below.</p>
-            <a style="appearance: button;" href=${
-                config.frontendUrl
-            }/activateAccount?token=${token}&id=${userId.toString()}>Activate Account</a>
+            <a style="appearance: button;" href=${config.frontendUrl}/activateAccount?token=${token}}>Activate Account</a>
             <p>Best,<br>The BMP Team
             </div>`,
         }
@@ -36,16 +34,14 @@ export default class MailingService {
         await this.transporter.sendMail(mailData)
     }
 
-    public async sendResetPasswordMail(to: string, token: string, userId: number): Promise<void> {
+    public async sendResetPasswordMail(to: string, token: string): Promise<void> {
         const mailData: any = {
             from: config.emailFrom,
             to: to,
             subject: 'Reset Password Request',
             html: `<h2 style="color: black;">Password Reset</h2>
             <p>We're sending you this email  because you requested a password reset. Click on this link to reset your password:</p>
-            <a style="appearance: button;" href=${
-                config.frontendUrl
-            }/resetPassword?token=${token}&id=${userId.toString()}>Reset Password</a>
+            <a style="appearance: button;" href=${config.frontendUrl}/resetPassword?token=${token}}>Reset Password</a>
             <p>Best,<br>The BMP Team
             </div>`,
         }

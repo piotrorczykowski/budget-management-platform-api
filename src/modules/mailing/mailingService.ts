c3be9f39691adf9ea016a61aba894c5f8469183b
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-06-03 10:18:51 +0200

    Add implementation for sending reset password mail

--- src/modules/mailing/mailingService.ts
+++ src/modules/mailing/mailingService.ts
@@ -19,14 +19,33 @@ export default class MailingService {
         })
     }
 
-    public async sendUserActivationMail(to: string, token: string): Promise<void> {
+    public async sendUserActivationMail(to: string, token: string, userId: number): Promise<void> {
         const mailData: any = {
             from: config.emailFrom,
             to: to,
             subject: 'Account Activation',
-            html: `<h1 style="color: black;">Almost done..</h1>
+            html: `<h2 style="color: black;">Almost done..</h2>
             <p>Thank you for registering with us. In order to activate your account please click the link below.</p>
-            <a style="appearance: button;" href=${config.frontendUrl}/activateAccount/${token}>Activate Account</a>
+            <a style="appearance: button;" href=${
+                config.frontendUrl
+            }/activateAccount?token=${token}&id=${userId.toString()}>Activate Account</a>
+            <p>Best,<br>The BMP Team
+            </div>`,
+        }
+
+        await this.transporter.sendMail(mailData)
+    }
+
+    public async sendResetPasswordMail(to: string, token: string, userId: number): Promise<void> {
+        const mailData: any = {
+            from: config.emailFrom,
+            to: to,
+            subject: 'Reset Password Request',
+            html: `<h2 style="color: black;">Password Reset</h2>
+            <p>We're sending you this email  because you requested a password reset. Click on this link to reset your password:</p>
+            <a style="appearance: button;" href=${
+                config.frontendUrl
+            }/resetPassword?token=${token}&id=${userId.toString()}>Reset Password</a>
             <p>Best,<br>The BMP Team
             </div>`,
         }
--- src/modules/mailing/types/index.ts
+++ src/modules/mailing/types/index.ts
@@ -1,4 +1,5 @@
-export type UserActivationMailData = {
+export type MailData = {
     to: string
     token: string
+    userId: number
 }

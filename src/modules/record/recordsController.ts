597ec7bdcc7ada66d40b905d34dab67f531908da
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-07-14 19:09:20 +0200

    Add implementation for records management

--- src/modules/record/recordsController.ts
+++ src/modules/record/recordsController.ts
@@ -1,4 +1,4 @@
-import { GET, POST, route } from 'awilix-koa'
+import { DELETE, GET, POST, PUT, route } from 'awilix-koa'
 import RecordsService from './recordsService'
 import { Context } from 'koa'
 import { RecordData } from './types'
@@ -11,11 +11,28 @@ export default class RecordsController {
         this.recordsService = recordsService
     }
 
+    // TODO add middleware for user access validation
     @route('/')
     @POST()
     public async createRecord(ctx: Context) {
         ctx.body = await this.recordsService.handleRecordCreation(<RecordData>ctx.request.body)
         ctx.status = 201
+    }
+
+    // TODO add middleware for user access validation
+    @route('/:recordId')
+    @PUT()
+    public async updateRecord(ctx: Context) {
+        ctx.body = await this.recordsService.handleRecordUpdate(ctx.params.recordId, <RecordData>ctx.request.body)
+        ctx.status = 200
+    }
+
+    // TODO add middleware for user access validation
+    @route('/:recordId')
+    @DELETE()
+    public async deleteRecord(ctx: Context) {
+        ctx.body = await this.recordsService.deleteRecord(ctx.params.recordId)
+        ctx.status = 204
     }
 
     // TODO add middleware for user access validation
--- src/modules/record/types/constants.ts
+++ src/modules/record/types/constants.ts
@@ -1,3 +1,1 @@
 export const CategoryNameToSkipInFilter: string = 'All'
-
-export const DefaultAccountName: string = 'Out Of the Platform'

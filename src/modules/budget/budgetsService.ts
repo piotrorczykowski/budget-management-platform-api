beacd626807c0dd556afc2418df29ac42ea606eb
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-07-15 14:15:02 +0200

    Add implementation for fetching paginated user budgets

--- src/modules/budget/budgetsService.ts
+++ src/modules/budget/budgetsService.ts
@@ -3,6 +3,8 @@ import Budget from '../../database/entities/Budget'
 import { BudgetData } from './types'
 import User from '../../database/entities/User'
 import moment from 'moment'
+import { PaginatedData } from './types'
+import { QueryOrder } from '@mikro-orm/core'
 
 export default class BudgetsService {
     budgetRepository: EntityRepository<Budget>
@@ -22,5 +24,43 @@ export default class BudgetsService {
 
         await this.budgetRepository.persistAndFlush(budget)
         return budget
+    }
+
+    public async getUserBudgets({
+        userId,
+        page,
+        pageSize,
+        searchByValue,
+    }: {
+        userId: number
+        page: number
+        pageSize: number
+        searchByValue: string
+    }): Promise<PaginatedData> {
+        const skipCount: number = (page - 1) * pageSize
+
+        const budgets: Budget[] = await this.budgetRepository.find(
+            {
+                name: { $like: `%${searchByValue}%` },
+                user: { id: userId },
+            },
+            {
+                orderBy: {
+                    endDate: QueryOrder.DESC,
+                },
+                offset: skipCount,
+                limit: pageSize,
+            }
+        )
+
+        const budgetsCount: number = await this.budgetRepository.count({
+            name: { $like: `%${searchByValue}%` },
+        })
+        const pageCount: number = Math.ceil(budgetsCount / pageSize)
+
+        return {
+            items: budgets,
+            pageCount: pageCount,
+        }
     }
 }

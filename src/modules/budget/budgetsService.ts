0909597d832bfee511dbbfa150959ac3819eac05
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-07-19 14:02:37 +0200

    Add updating budget progress & minor changes

--- src/modules/budget/budgetsService.ts
+++ src/modules/budget/budgetsService.ts
@@ -5,22 +5,33 @@ import User from '../../database/entities/User'
 import moment from 'moment'
 import { PaginatedData } from './types'
 import { QueryOrder } from '@mikro-orm/core'
+import BudgetRecordsService from './budgetRecordsService'
 
 export default class BudgetsService {
     budgetRepository: EntityRepository<Budget>
+    budgetRecordsService: BudgetRecordsService
 
-    constructor({ budgetRepository }: { budgetRepository: EntityRepository<Budget> }) {
+    constructor({
+        budgetRepository,
+        budgetRecordsService,
+    }: {
+        budgetRepository: EntityRepository<Budget>
+        budgetRecordsService: BudgetRecordsService
+    }) {
         this.budgetRepository = budgetRepository
+        this.budgetRecordsService = budgetRecordsService
     }
 
     public async createBudget(user: User, budgetData: BudgetData): Promise<Budget> {
-        const budget: Budget = new Budget()
+        let budget: Budget = new Budget()
         budget.name = budgetData.name
         budget.planned = budgetData.planned
-        budget.startDate = moment(budgetData.startDate).utc().toDate()
-        budget.endDate = moment(budgetData.endDate).utc().toDate()
+        budget.startDate = moment(budgetData.startDate).utc().startOf('day').toDate()
+        budget.endDate = moment(budgetData.endDate).utc().endOf('day').toDate()
         budget.categories = budgetData.categories
         budget.user = user
+
+        budget = await this.budgetRecordsService.updateBudgetProgress(budget)
 
         await this.budgetRepository.persistAndFlush(budget)
         return budget

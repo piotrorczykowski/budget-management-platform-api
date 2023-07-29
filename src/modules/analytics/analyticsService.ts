4111ab897fd8e2831fc7626fda431b03613f9499
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-07-29 14:31:59 +0200

    Add first version of job saving historical account balance

--- src/modules/analytics/analyticsService.ts
+++ src/modules/analytics/analyticsService.ts
@@ -3,12 +3,26 @@ import Record from '../../database/entities/Record'
 import _ from 'lodash'
 import moment from 'moment'
 import { CashFlowType, ExpensesStructureType } from './types'
+import Account from '../../database/entities/Account'
+import HistoricalAccountBalance from '../../database/entities/HistoricalAccountBalance'
 
 export default class AnalyticsService {
     recordRepository: EntityRepository<Record>
+    accountRepository: EntityRepository<Account>
+    historicalAccountBalanceRepository: EntityRepository<HistoricalAccountBalance>
 
-    constructor({ recordRepository }: { recordRepository: EntityRepository<Record> }) {
+    constructor({
+        recordRepository,
+        accountRepository,
+        historicalAccountBalanceRepository,
+    }: {
+        recordRepository: EntityRepository<Record>
+        accountRepository: EntityRepository<Account>
+        historicalAccountBalanceRepository: EntityRepository<HistoricalAccountBalance>
+    }) {
         this.recordRepository = recordRepository
+        this.accountRepository = accountRepository
+        this.historicalAccountBalanceRepository = historicalAccountBalanceRepository
     }
 
     public async getCashFlow(userId: number, monthForInfo: Date): Promise<CashFlowType> {
@@ -81,5 +95,40 @@ export default class AnalyticsService {
         })
 
         return expensesStructure
+    }
+
+    public async saveHistoricalAccountBalanceJob(): Promise<void> {
+        const accounts: Account[] = await this.accountRepository.find(
+            {},
+            {
+                populate: ['historicalAccountBalances'],
+            }
+        )
+
+        const historicalAccountBalances: HistoricalAccountBalance[] = []
+
+        // TODO change 0 to 1
+        for (const account of accounts) {
+            const historicalBalance: HistoricalAccountBalance = Array.from(account.historicalAccountBalances)?.find(
+                (historicalBalance) =>
+                    moment(historicalBalance.date).utc().isSame(moment().subtract(0, 'month').endOf('month'), 'month')
+            )
+            const historicalBalanceAlreadyExists: boolean = !!historicalBalance
+
+            if (historicalBalanceAlreadyExists) {
+                historicalBalance.balance = account.balance
+                historicalAccountBalances.push(historicalBalance)
+            } else {
+                const historicalAccountBalance: HistoricalAccountBalance = new HistoricalAccountBalance()
+
+                historicalAccountBalance.balance = account.balance
+                historicalAccountBalance.date = moment().subtract(0, 'month').endOf('month').toDate()
+                historicalAccountBalance.account = account
+
+                historicalAccountBalances.push(historicalAccountBalance)
+            }
+        }
+
+        await this.historicalAccountBalanceRepository.persistAndFlush(historicalAccountBalances)
     }
 }
